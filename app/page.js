9fc89a436a74858a38bc956e1ed7c531e666fd64
Gagandeep9354 <sgagan9354@gmail.com>
2024-10-26 19:21:31 -0600

    Added the link to different week demos

--- app/page.js
+++ app/page.js
@@ -6,10 +6,22 @@ export default function Page() {
       <h1>Web Dev 2 Demos</h1>
       <p>Demos for Web Dev 2</p>
       <li>
-        <Link href="/week-2">Week 2 Demos</Link>
+        <Link href="/week-2">Week 2 Demo</Link>
       </li>
       <li>
-        <Link href="/week-3">Week 3 Demos</Link>
+        <Link href="/week-3">Week 3 Demo</Link>
+      </li>
+      <li>
+        <Link href="/week-4">Week 4 Demo</Link>
+      </li>
+      <li>
+        <Link href="/week-5">Week 5 Demo</Link>
+      </li>
+      <li>
+        <Link href="/week-7">Week 7 Demo</Link>
+      </li>
+      <li>
+        <Link href="/week-9">Week 9 Demo</Link>
       </li>
     </div>
   );
